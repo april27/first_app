02a9aec26a5776f6488cae29c7410a80dca86da3
U-INTERNAL\xieyuan <xieyuan@xieyuan.internal.baidu.com>
2011-10-16 16:43:12 +0800

    initial ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a7a36667068310e8dc15ffb501b87f92d22dd8289cbb7b7ff0e8c296e168dc7f2009f581542d6518a036ff504c4c203c36c86c4b377005ff20df3c9aa0076347'
